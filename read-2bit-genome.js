8d768e1a90eb914518625c52db5d11145064f058
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-01-04 17:24:22 +0900

    studying 16-byte headers

--- read-2bit-genome.js
+++ read-2bit-genome.js
@@ -1,21 +1,58 @@
 // import fs from 'fs';
 const fs = require('fs')
 
-let binary = fs.readFileSync('/Users/imamachinaoto/Desktop/hg38.2bit');
-
-let header = binary.slice(0, 16);
-
-let headerUint8 = new Uint8Array(header);
-
+const hg38_filepath = '/Users/imamachinaoto/Desktop/hg38.2bit';
 // The file begins with a 16-byte header containing the following fields:
 // signature - the number 0x1A412743 in the architecture of the machine that created the file
-const TWOBIT_GENOME_SIGNATURE = 0x1A412743
-const TWOBIT_GENOME_SIGNATURE_NEW = 0x4327411A
+const TWOBIT_GENOME_SIGNATURE = 0x1A412743;
+let byteswapped = false;
 
-// version - zero for now. Readers should abort if they see a version number higher than 0.
-// sequenceCount - the number of sequences in the file.
-// reserved - always zero for now
+// callback(err: NodeJS.ErrnoException, data: Buffer): void
+fs.readFile(hg38_filepath, (err, data) => {
+  // Error handling
+  if (err) {
+    throw err;
+  }
 
+  // Get twobit genome signature
+  let header = data.slice(0, 16);
+  console.log(header);
+
+  // Check little endian or big endian
+  if (header.readUInt32LE(0, 4) === TWOBIT_GENOME_SIGNATURE) {
+    // If header is little endian
+    byteswapped = true;
+    console.log("This file is LE");
+  } else if (header.readUInt32BE(0, 4) === TWOBIT_GENOME_SIGNATURE) {
+    // If header is big endian
+    byteswapped = false;
+    console.log("This file is BE");
+  } else {
+    throw new Error('Not a .2bit file.\n' + 'File Signature: ' + header + " not " + TWOBIT_GENOME_SIGNATURE);
+  }
+
+  // version - zero for now. Readers should abort if they see a version number higher than 0.
+  if (header.readUInt32LE(4 * 1, 4 * 2) !== 0x00000000) {
+    throw Error('File version in header should be 0.');
+  } else {
+    console.log('Version: OK!');
+  }
+
+  // sequenceCount - the number of sequences in the file.
+  let sequenceCount = header.slice(4 * 2, 4 * 3).readUIntBE(0, 4);
+  console.log(sequenceCount);
+
+  // reserved - always zero for now
+  if (header.readUInt32LE(4 * 3, 4 * 4) !== 0x00000000) {
+    throw Error('Reserved field in header should be 0');
+  } else {
+    console.log('Reversed: OK!');
+  }
+});
+
+const buf = Buffer.from([0xc7, 0x01, 0x00, 0x00]);
+console.log(buf.readUIntBE(0, 4));
+let headerUint8 = new Uint8Array(header);
 
 
 const twoBit = ['T', 'C', 'A', 'G']
